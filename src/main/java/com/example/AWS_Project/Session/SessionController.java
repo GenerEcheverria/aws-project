d2f6f724aa87890f1e02d7cdf1903d2f575e3eb0
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-12-09 13:47:08 -0600

    logout

--- src/main/java/com/example/AWS_Project/Session/SessionController.java
+++ src/main/java/com/example/AWS_Project/Session/SessionController.java
@@ -28,7 +28,7 @@ public class SessionController {
       this.alumnoService = alumnoService;
    }
 
-   @PostMapping(path = "/alumnos/{id}/session/login")
+   @PostMapping(path = "/alumnos/{id}/session/login", consumes = "application/json", produces = "application/json")
    public ResponseEntity<Object> login(@PathVariable("id") int id, @RequestBody Map<String, String> password) {
       try {
          Optional<Alumno> alumnoOptional = alumnoService.getAlumnoById(id);
@@ -53,29 +53,44 @@ public class SessionController {
       }
    }
 
-   @PostMapping(path = "/alumnos/{id}/session/verify")
+   @PostMapping(path = "/alumnos/{id}/session/verify", consumes = "application/json", produces = "application/json")
    public ResponseEntity<Object> verify(@PathVariable("id") int id, @RequestBody Map<String, String> sessionString) {
       try {
          Optional<Session> optionalSession = sessionRepository.findByAlumnoId(id);
          if (optionalSession.isPresent()) {
             Session session = optionalSession.get();
             if (session.getSessionString().equals(sessionString.get("sessionString")) && session.isActive()) {
-                return new ResponseEntity<>("Session is valid", HttpStatus.OK);
+               return new ResponseEntity<>("Session is valid", HttpStatus.OK);
             } else {
-                return new ResponseEntity<>("Session is invalid", HttpStatus.BAD_REQUEST);
+               return new ResponseEntity<>("Session is invalid", HttpStatus.BAD_REQUEST);
             }
-        } else {
+         } else {
             return new ResponseEntity<>("No session found for the specified alumnoId", HttpStatus.NOT_FOUND);
-        }
+         }
+      } catch (RuntimeException e) {
+         return new ResponseEntity<>("Server error", HttpStatus.INTERNAL_SERVER_ERROR);
+      }
+   }
+
+   @PostMapping(path = "/alumnos/{id}/session/logout", consumes = "application/json", produces = "application/json")
+   public ResponseEntity<Object> logout(@PathVariable("id") int id, @RequestBody Map<String, String> sessionString) {
+      try {
+         Optional<Session> optionalSession = sessionRepository.logout(id, sessionString.get("sessionString"));
+         if (optionalSession.isPresent()) {
+            Session session = optionalSession.get();
+            return new ResponseEntity<>(session, HttpStatus.OK);
+         } else {
+            return new ResponseEntity<>("No session found for the specified alumnoId", HttpStatus.NOT_FOUND);
+         }
       } catch (RuntimeException e) {
          return new ResponseEntity<>("Server error", HttpStatus.INTERNAL_SERVER_ERROR);
       }
    }
 
    private String generateRandomString(int length) {
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] randomBytes = new byte[length / 2];
-        secureRandom.nextBytes(randomBytes);
-        return new BigInteger(1, randomBytes).toString(16);
-    }
+      SecureRandom secureRandom = new SecureRandom();
+      byte[] randomBytes = new byte[length / 2];
+      secureRandom.nextBytes(randomBytes);
+      return new BigInteger(1, randomBytes).toString(16);
+   }
 }
--- src/main/java/com/example/AWS_Project/Session/SessionRepository.java
+++ src/main/java/com/example/AWS_Project/Session/SessionRepository.java
@@ -9,12 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
-import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
-import com.amazonaws.services.dynamodbv2.datamodeling.PaginatedQueryList;
 import com.amazonaws.services.dynamodbv2.model.AttributeValue;
-import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
 
 @Repository
 public class SessionRepository {
@@ -36,5 +32,27 @@ public class SessionRepository {
         return sessions.isEmpty() ? Optional.empty() : Optional.of(sessions.get(sessions.size() - 1));
     }
 
+    public Optional<Session> logout(int alumnoId, String sessionString) {
+        Map<String, AttributeValue> eav = new HashMap<>();
+        eav.put(":val1", new AttributeValue().withN(Integer.toString(alumnoId)));
+    
+        DynamoDBScanExpression scanExpression = new DynamoDBScanExpression()
+            .withFilterExpression("alumnoId = :val1")
+            .withExpressionAttributeValues(eav);
+    
+        List<Session> sessions = dynamoDBMapper.scan(Session.class, scanExpression);
+    
+        if (!sessions.isEmpty()) {
+            Session sessionToUpdate = sessions.get(sessions.size() - 1);
+            System.out.println(sessionToUpdate);
+            System.out.println(sessionString);
+            if (sessionToUpdate.getSessionString().equals(sessionString)) {
+                sessionToUpdate.setActive(false);
+                dynamoDBMapper.save(sessionToUpdate);
+                return Optional.of(sessionToUpdate);
+            }
+        }
+        return Optional.empty();
+    }
     
 }
