6db4174b4eae25e8cee9ee8e56bce767bdfeb821
Gener Echeverría <56045434+GenerEcheverria@users.noreply.github.com>
2023-12-12 17:32:15 -0600

    Update Credentials.java

--- src/main/java/com/example/AWS_Project/Credentials.java
+++ src/main/java/com/example/AWS_Project/Credentials.java
@@ -1,7 +1,7 @@
 package com.example.AWS_Project;
 
 public class Credentials {
-    public static final String ACCESS_KEY = "ASIAZ3YIADFGUERRA65H";
-    public static final String SECRET_KEY = "arSyv9jE98d7FLGZJI26t95zVvUYjFrZQvXyM8a9";
-    public static final String SESSION_TOKEN = "FwoGZXIvYXdzEI3//////////wEaDFTjMKcdSX43jS9tgiLPAYWMU4gU5x5S5/d2mrkqEs/zFoZH8g6PMa/Xzh8/1Ns6hLKp+Wkm0qnGsarRXEJRAwAZzTG2gDjkHwYgC86AaoyS5hOnBhpowI3y7wJlBClDGOra6kRyCbFEPkSnPuZpuJWqJbRW1cNlfbVrZXTFGyb4C1pCd/m4mRM5XO//rIgpDaopG/u166pl2xF0j7opHvhXYF+hzgjJU+xa71lgkUeJq9HX5kmMG/JEmBPNxT6WPV2ZMKNzoD375GoHsvrAt+7K4Y6VwMvevJIuS7iQcijz29SrBjIt8LhSpA+V6me4OkNU1H8WQ/TKzGmJKviDCh5M1O6l93ABJ0kq0/MYFpIZ1KWF";
+    public static final String ACCESS_KEY = "ASIAZ3YIADFGQIPXLIB4";
+    public static final String SECRET_KEY = "ilbhBoCx2En1R8qFzJoyLcSDUxNd8qME+Kfom7Y3";
+    public static final String SESSION_TOKEN = "FwoGZXIvYXdzENH//////////wEaDOBlx66VnC1Eh4n1xyLPAb5Hv1K64y1DTfESa+I4E3J3vptLuE+C0mAOWoNEZIsAI7sx4Xbx+kTDGfuCzBHMlgRNJ26cNyloHciIZujyl6RFJet/dxX15YfHXOsRwCo3Ew7OU7SofYu/BIiS8WOovjEotr/w22HxjI8NAy6GvovpexZxCZpONzL6znZilm7ioIy8NBvxOdDbbmr/W7ISWufNhhcWQENZRFF2m0ItEliwJDbpYbLQwBXbES6xF6gyciKnXSEluJob428fsl9t/d5IwtnXhUz1oqAyeew5kijr1+OrBjIt8acu3dV1zNmlf68l+c2uuUtJMH1Z+EtRIi1zTIWJpzc8erV5F9YMzfnlBem8";
 }
